d9337bd26d809c59f5d10bc34096de23f705c6b0
venkiradkris <venkiradkris@gmail.com>
2021-03-31 09:37:51 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/DHLVenkar-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name = "Terraform Azure RM Module for Network"
+  resource_group_name = var.resource_group_name
 }
